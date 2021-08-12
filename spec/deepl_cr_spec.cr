b51ddb213d9bf049f21c489d376153fa2ded7fc4
Anton Maminov <anton.linux@gmail.com>
2021-08-13 02:00:03 +0300

    add basic spec

--- spec/deepl_cr_spec.cr
+++ spec/deepl_cr_spec.cr
@@ -1,9 +1,18 @@
 require "./spec_helper"
 
-describe DeeplCr do
-  # TODO: Write tests
+describe Deepl do
+  it "translate" do
+    WebMock.stub(:post, "https://api-free.deepl.com/v2/translate")
+      .with(body: "text=hello&source_lang=EN&target_lang=DE", headers: {"Authorization" => "DeepL-Auth-Key key:fx"})
+      .to_return(status: 200, body: {"translations" => [{"detected_source_language" => "EN", "text" => "Hallo"}]}.to_json)
 
-  it "works" do
-    false.should eq(true)
+    Deepl::Dispatcher.configure do |settings|
+      settings.api_key = "key:fx"
+    end
+
+    Habitat.raise_if_missing_settings!
+
+    resp = Deepl::Dispatcher.translate("hello", from: "EN", to: "DE")
+    resp.translations.should eq([{"detected_source_language" => "EN", "text" => "Hallo"}])
   end
 end
